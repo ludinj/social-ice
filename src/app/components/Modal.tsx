aa22f3238ce409e89e34747546e17a9987f91b6b
Ludin Jaimes <borrego300@yahoo.com>
2023-07-24 21:16:52 -0400

    Moved Modal position to the top

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -42,7 +42,7 @@ const Modal: FC<ModalProps> = ({
     setShowModal(false);
     setTimeout(() => {
       onClose();
-    }, 300);
+    }, 200);
   }, [disable, onClose]);
 
   const handleSubmit = useCallback(() => {
@@ -55,14 +55,14 @@ const Modal: FC<ModalProps> = ({
   }
   return (
     <>
-      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/6 my-6 mx-auto h-full lg:h-auto md:h-auto">
+      <div className="flex justify-center w-full  overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+        <div className="relative mt-28 w-full md:w-4/6 lg:w-3/6 xl:w-2/6 my-6 mx-auto h-full lg:h-auto md:h-auto">
           {/* CONTENT */}
           <div
             className={`
-            duration-300
+            duration-200
             h-full
-            ${showModal ? 'translate-y-0' : 'translate-y-full'}
+            ${showModal ? 'translate-y-0' : 'translate-y-[20%]'}
             ${showModal ? 'opacity-100' : 'opacity-0'}`}
           >
             <div className="h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
